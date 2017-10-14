440b9509c4a7ec8765f65ab8a4ce4d09fe56c83d
Cants820 <Cantosjoseph@gmail.com>
2017-10-14 14:53:38 -0700

    files added

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '3Kc3UxcNZJJwyLQkvFQAJ8DYA',
+  consumer_secret: 'P9BIQiW1c7EstluQKBnVHRm0IOcTXK5Nm7CYP0q2KSk8zmumU8',
+  access_token_key: '1353115074-b55Z0wPPrDcS4X4s41mqWsw0dnqG3HXPm1Bo5fx',
+  access_token_secret: 'L5LWsszwLW8W4AG2mMOolBfdjmw1D20tBzBZbdkpHO50t',
+}
+
+module.exports = twitterKeys;
